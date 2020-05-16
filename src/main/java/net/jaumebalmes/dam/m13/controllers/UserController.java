f029652517e9dee46b3199efc4b92d4efbeb0558
jordi <jperez18@jaumebalmes.net>
2020-05-16 17:34:12 +0200

    change h2 configuration adds CORS policy

--- src/main/java/net/jaumebalmes/dam/m13/controllers/UserController.java
+++ src/main/java/net/jaumebalmes/dam/m13/controllers/UserController.java
@@ -3,11 +3,9 @@ package net.jaumebalmes.dam.m13.controllers;
 import net.jaumebalmes.dam.m13.dao.UserRepository;
 import net.jaumebalmes.dam.m13.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 public class UserController {
     @Autowired
