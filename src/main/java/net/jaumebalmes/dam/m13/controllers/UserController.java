59638717097fe2c679d05c1bd569b837cf265e88
jordi <jperez18@jaumebalmes.net>
2020-05-26 17:22:08 +0200

    adds salt string to User entity adds encryption password to newUser method adds login with encrypt password

--- src/main/java/net/jaumebalmes/dam/m13/controllers/UserController.java
+++ src/main/java/net/jaumebalmes/dam/m13/controllers/UserController.java
@@ -3,6 +3,7 @@ package net.jaumebalmes.dam.m13.controllers;
 import net.jaumebalmes.dam.m13.dao.UserRepository;
 import net.jaumebalmes.dam.m13.entities.LoginForm;
 import net.jaumebalmes.dam.m13.entities.User;
+import net.jaumebalmes.dam.m13.utils.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,10 @@ public class UserController {
         if(optionalUser.isPresent()){
             return ResponseEntity.status(409).body("409");
         }
+        String salt = PasswordUtils.getSalt(30);
+        String securePassword = PasswordUtils.generateSecurePassword(newUser.getPassword(),salt);
+        newUser.setPassword(securePassword);
+        newUser.setSalt(salt);
         userRepository.save(newUser);
         return ResponseEntity.status(200).body("User creat amb id "+newUser.getId());
     }
@@ -29,38 +34,24 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<Integer> logInUser(@RequestBody LoginForm loginForm){
         HttpHeaders responseHeaders = new HttpHeaders();
-        Optional<User> optUser = userRepository.findByEmailAndPassword(loginForm.getEmail(), loginForm.getPassword());
+        //Optional<User> optUser = userRepository.findByEmailAndPassword(loginForm.getEmail(), loginForm.getPassword());
+        Optional<User> optUser = userRepository.findByEmail(loginForm.getEmail());
         if(optUser.isPresent()){
             User user = optUser.get();
-            long id = user.getId();
-            int status = 200;
-            String statusTotal = ""+status+id;
-            responseHeaders.set("Allow-SymPass-Access","2");
-            return ResponseEntity.status(Integer.parseInt(statusTotal)).body(2);
-        }
-        optUser = userRepository.findByEmail(loginForm.getEmail());
-        if(optUser.isPresent()){
-            responseHeaders.set("Allow-SymPass-Access","1");
-            return ResponseEntity.status(406).body(1);
+            boolean passwordMatch = PasswordUtils.verifyUserPassword(loginForm.getPassword(),user.getPassword(),user.getSalt());
+            if(passwordMatch){
+                long id = user.getId();
+                int status = 200;
+                String statusTotal = ""+status+id;
+                responseHeaders.set("Allow-SymPass-Access","2");
+                return ResponseEntity.status(Integer.parseInt(statusTotal)).body(2);
+            }else{
+                responseHeaders.set("Allow-SymPass-Access","1");
+                return ResponseEntity.status(406).body(1);
+            }
         }else{
             responseHeaders.set("Allow-SymPass-Access","0");
             return ResponseEntity.status(401).body(0);
         }
-
     }
-
-    @GetMapping("/user")
-    public Iterable<User> getUserList(){
-        return userRepository.findAll();
-    }
-
-    /*@PostMapping("/checkEmail")
-    public ResponseEntity<Integer> checkExistenceEmail(@RequestBody LoginForm loginForm){
-        Optional<User> optionalUser = userRepository.findByEmail(loginForm.getEmail());
-        if(optionalUser.isPresent()){
-            return ResponseEntity.status(409).body(409);
-        }else{
-            return ResponseEntity.status(200).body(200);
-        }
-    }*/
 }
